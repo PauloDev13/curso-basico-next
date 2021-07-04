91faea2a05235a452011fb9c8ef4f5ebfd95ee16
PauloDev <devpgm2020@gmail.com>
2021-07-03 23:53:10 -0300

    Adicionando sass ao projeto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
   return (
     <div >
-      <h1>Olá Dev</h1>
+      <h1 className={styles.title}>Olá <span>Dev</span></h1>
     </div>
   )
 }
