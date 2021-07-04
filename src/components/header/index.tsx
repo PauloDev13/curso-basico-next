bab4a0cab8eec42bedbf736e2f611c2678e94841
PauloDev <devpgm2020@gmail.com>
2021-07-04 12:26:51 -0300

    Implemtando o componente ActiveLink e refatorando o código do componente Header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,7 @@
 import styles from './header.module.scss';
-import Link from 'next/link';
+// import Link from 'next/link';
 import { useRouter } from 'next/router';
+import ActiveLink from '../ActiveLink';
 
 export function Header() {
   const { asPath } = useRouter();
@@ -9,12 +10,18 @@ export function Header() {
       <div className={styles.content}>
         <img src="/logo.svg" alt="DevNews" />
         <nav>
-          <Link href="/">
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>{' '}
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
+          {/*<Link href="/">
             <a className={asPath === '/' ? styles.active : ''}>Home</a>
           </Link>
           <Link href="/posts">
-            <a className={asPath === '/posts' ? styles.active : ''}>Posts</a>
-          </Link>
+            <a className={asPath === '/posts' ? styles.active : ''}>Posts</a>*
+          </Link>*/}
         </nav>
       </div>
     </header>
