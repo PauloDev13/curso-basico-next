a1ab8ef6e42a7fb0e891f8dad49c8cd74f74c4dd
PauloDev <devpgm2020@gmail.com>
2021-07-04 13:35:33 -0300

    Usando Json Server e Renderizando dados Client Side Rendering"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,34 @@
-import styles from '../styles/home.module.scss';
+import { useEffect, useState } from 'react';
+
+interface Post {
+  id: string;
+  title: string;
+}
 
 export default function Home() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  const getPosts = async () => {
+    const dados = await fetch('http://localhost:3333/posts');
+    const data = await dados.json();
+    setPosts(data);
+    // console.log(data);
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
-    <div >
-      <h1>Olá Dev</h1>
+    <div>
+      <div>
+        <h1>Posts</h1>
+        <ul>
+          {posts.map(post => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
